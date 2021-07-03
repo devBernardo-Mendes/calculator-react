19aa50dfa3d8f18fe4f885bc9860982853e72c30
bernardo-mr1 <devbernardomr@gmail.com>
2021-07-03 20:20:07 -0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/Calculator';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
