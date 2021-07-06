0ff3c5615def99e4c696faefffbf2d441254cf7c
bernardo-mr1 <devbernardomr@gmail.com>
2021-07-06 18:09:39 -0300

    feat: finish my calculator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,12 +8,14 @@ export default props => {
     classes += props.triple ? 'triple' : ''
     return (
         <button 
-            onClick={e => props.click && props.click(e.target.label)}
+            onClick={e => props.click && props.click(props.label)}
             className={classes}>
             {props.label}
         </button>
     )
 }
+    
+
 
 
 
